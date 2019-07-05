8363d1610d43c94a3d172701653fd436d5b4fe51
Fabian Hiller <gcode@fabianhiller.de>
2019-07-05 18:54:17 +0200

    Updates

---
+++ precache-manifest.0361ba66528c9563a3198eb19e1e8e73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af8e9485a30071c4c34915b696e44a3",
+    "url": "/gcode-viewer/index.html"
+  },
+  {
+    "revision": "752579e8fd9705cc07e1",
+    "url": "/gcode-viewer/static/css/2.3547d183.chunk.css"
+  },
+  {
+    "revision": "752579e8fd9705cc07e1",
+    "url": "/gcode-viewer/static/js/2.ab8ac619.chunk.js"
+  },
+  {
+    "revision": "c4f4d1dd410f8032ceea",
+    "url": "/gcode-viewer/static/js/main.5ae7e965.chunk.js"
+  },
+  {
+    "revision": "c9a9da2fa084c2d6d6e8",
+    "url": "/gcode-viewer/static/js/runtime~main.e16276b0.js"
+  }
+]);
